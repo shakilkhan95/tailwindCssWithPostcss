8934cb03413733135f10d31aa8b68afe840e6d17
shakilkhan95 <th.shakilkhan@gmail.com>
2024-11-10 04:35:58 +0400

    function

---
+++ script.js
@@ -0,0 +1,22 @@
+// // function for darkMode
+// const moon = document.querySelector('#moon');
+// const htmlTag = document.querySelector('#htmlTag');
+
+// moon.addEventListener('click', function() {
+//     htmlTag.classList.toggle('dark');
+// });
+
+
+// // function for Mobile screen navbar
+// const menuBar = document.querySelector('#menuBar');
+// const navBar = document.querySelector('#navBar');
+// const navLink = document.querySelector('#navBar > li');
+
+// function toggle() {
+//     navBar.classList.toggle('hidden');
+// }
+
+// menuBar.addEventListener('click', toggle);
+// nav.forEach(li => {
+//     li.addEventListener('click', toggle);
+// });
