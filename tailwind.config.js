2b4b23fc01b0b51d81b09f130eee080eeb17121b
shakilkhan95 <th.shakilkhan@gmail.com>
2024-11-10 04:20:58 +0400

    dd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': '"Poppins", serif'
+      },
+
+      colors: {
+        'main-color': '',
+        'sec-color': '',
+        'text-color': '',
+        'other-color': ''
+      }
+    },
   },
   plugins: [],
 }
